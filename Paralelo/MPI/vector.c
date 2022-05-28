bc0075d5e308b0d28a4b3805eef3c5a4491989f3
Federico <fedeadra@gmail.com>
2022-05-28 17:59:01 -0300

    Empiezo a implementar el vector, quedan los broadcasts y otras cosas

--- Paralelo/MPI/vector.c
+++ Paralelo/MPI/vector.c
@@ -1,6 +1,9 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+#define ROOT_P 0
 #ifndef USE_FLOAT
     #define DATA_T double
     #define MPI_DATA_T MPI_DOUBLE
@@ -9,17 +12,20 @@
     #define MPI_DATA_T MPI_FLOAT
 #endif
 
+#define precision 0.01
+
+//Utils
 double dwalltime();
+void printVector(int N, DATA_T *M);
+
 int funcionSlave(int, int, int);
 int funcionDelMaster(int N, int P);
-
 
 int main(int argc, char **argv)
 {
     int miID;
     int nrProcesos;
     int N;
-    double timetick;
 
     MPI_Init(&argc, &argv);                     //Inicializa el ambiente
     MPI_Comm_rank(MPI_COMM_WORLD, &miID);       // Obtiene el identificador de cada proceso (rank)
@@ -53,40 +59,152 @@ int main(int argc, char **argv)
     return (0);
 }
 
-int funcionDelMaster(int N, int P)
+int funcion(int id, int N, int numP){
+    DATA_T *A, *B;
+    int *convergeP; //Esto aca porque si no no compila ;p
+    double timetick; //Esto aca porque si no no compila ;p
+    if(id==ROOT_P){
+        A = (DATA_T *)malloc(sizeof(DATA_T) * N);
+        convergeP = (int *)malloc(sizeof(int)*numP);
+        double timetick;
+        //Inicialización del vector
+        int i,start,end,converge=0,offset;
+        for(i=0;i<N;i++) {
+            A[i] = randFP(0.0,1.0);
+        }
+        #ifdef DEBUG
+        printVector(N,A);
+        #endif
+        //Inicialización del converge
+        for(i=0;i<numP;i++){
+            convergeP[i]=0;
+        }
+    }
+
+    DATA_T *part = (DATA_T *)malloc(sizeof(DATA_T) * N/numP);
+    DATA_T *partB = (DATA_T *)malloc(sizeof(DATA_T) * N/numP);
+    DATA_T *swapAux;
+    int ant,sig,start,end;
+    int i,converge=0;
+    DATA_T primerElem;
+    start=id*N/numP + id==ROOT_P;
+    end=(id+1)*N/numP - id==numP;
+    MPI_Scatter(A,N/numP,MPI_DATA_T,part+1,N/numP,MPI_DATA_T,ROOT_P,MPI_COMM_WORLD);
+    //Algoritmo, primero broadcast del primer elemento    
+    if(id==ROOT_P){
+        timetick = dwalltime(); //Arranco a contar.
+        primerElem = (part[0]+part[1])*0.5;
+        //BROADCAST DE PRIMERELEM!! HACER
+    }
+    else{
+        //Recibo el primerElem
+        //HACER
+        //primerElem = ???
+    }
+    while(!converge){
+        converge=1;
+        //Enviamos su vecindad los threads (Cambiar por master enviando esto?)
+        MPI_Send(part,1, MPI_DATA_T, id-1, 99, MPI_COMM_WORLD); //Envio mi primer elemento a thread anterior part[0]
+        MPI_Send(part+(N/numP)-1,1, MPI_DATA_T, id+1, 99, MPI_COMM_WORLD); //Envio mi ultimo elemento a thread siguiente part[N/numP-1]
+        MPI_Recv(&ant,1,MPI_DATA_T,id-1,99,MPI_COMM_WORLD,MPI_STATUS_IGNORE); //Recibo el ultimo elemento del thread anterior
+        MPI_Recv(&sig,1,MPI_DATA_T,id-1,99,MPI_COMM_WORLD,MPI_STATUS_IGNORE); //Recibo el primer elemento del thread siguiente
+        //part -> arreglo con mi parte
+        //ant -> Elem ultimo de thread anterior
+        //sig -> Elem primero de thrad siguiente
+
+
+        //Calculo mi parte
+        for(i=start;i<N/numP;i++){
+            partB[i] = (part[i-1] + part[i] + part[i+1]) * (1.0/3); 
+            if (fabs(primerElem-partB[i]) > precision){
+                converge=0;
+                break;
+            }
+        }
+        //Calculo el promedio de los numeros que me faltaron antes de descubrir la divergencia
+        for (;i<end;i++){
+            partB[i] = (part[i-1] + part[i] + part[i+1])* (1.0/3);
+        }
+        if(id==numP){
+            partB[N-1] = (part[N-2]+part[N-1]) *0.5;
+			if (converge && fabs(primerElem-partB[i])>precision){
+				converge = 0;
+			}
+        }
+        if(id!=0){
+            //Si no soy el master envio la booleana de convergencia y sigo
+            MPI_Send(&converge,1,MPI_INT,ROOT_P, 99, MPI_COMM_WORLD); //Envio mi ultimo elemento a thread siguiente part[N/numP-1]
+            MPI_Recv(&converge,1,MPI_INT,ROOT_P, 99,MPI_COMM_WORLD,MPI_STATUS_IGNORE); //Recibo el ultimo elemento del thread anterior
+            //primerElem = ???? RECIBO BROADCAST DE MASTER
+            part=swapAux;
+            part=partB;
+            partB=swapAux;
+            continue;
+        }
+        //Soy el master, verifico la convergencia
+        for(i=1;i<numP;i++){ //i = id de proceso que recibo
+            MPI_Recv(convergeP+i,1,MPI_INT,i, 99,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            converge=converge && convergeP[i];
+        }
+        //Ver de hacer un broadcast con el valor B[0] y converge
+        primerElem = (part[0]+part[1])*0.5;
+        //BROADCAST PRIMER ELEM
+        //BROADCAST DE CONVERGE
+
+    }
+    MPI_Gather(part,N/numP,MPI_DATA_T,A,N/numP,MPI_DATA_T,0,MPI_COMM_WORLD);
+    if(id==ROOT_P){
+        printf("Tiempo en segundos %f\n", dwalltime() - timetick);
+    }
+    free(A);
+    free(B);
+    return 0;
+}
+
+int funcionDelMaster(int N, int numP)
 {
     DATA_T *A, *B;
     A = (DATA_T *)malloc(sizeof(DATA_T) * N);
     B = (DATA_T *)malloc(sizeof(DATA_T) * N);
-    int converge[T];
+    int converge2[numP];
+    double timetick;
+
     //Inicialización del vector
-	int i,start,end,converge=0;
+	int i,start,end,converge=0,offset;
 	for(i=0;i<N;i++) {
 		A[i] = randFP(0.0,1.0);
 	}
 	#ifdef DEBUG
 	printVector(N,A);
 	#endif
+    
     //Inicialización del converge
-    for(i=0;i<P;i++){
-        converge[i]=0;
+    for(i=0;i<numP;i++){
+        converge2[i]=0;
     }
 
+
+    //Algoritmo principal
     timetick = dwalltime();
+    B[0] = (A[0]+A[1])*0.5; //Calculo primer elemento antes de entrar
     while(!converge){
-        for (i = 1; i < P; i++)
+        /*Sin scatter
+        for (i = 1; i < numP; i++)
         {
+            offset = i*N/numP;
             //*Enviar B[0]*
-            MPI_Send(&B[0],MPI_DATA_T,i,99,MPI_COMM_WORLD); //revisar
+            MPI_Send(&B[0],1,MPI_DATA_T,i,99,MPI_COMM_WORLD);
             //*Enviar A para calcular*
-            MPI_Send(&(A[i * (N * (N / P))]), N * (N / P), MPI_DATA_T, i, 99, MPI_COMM_WORLD);
+            MPI_Send(A+(i*N/numP), N / numP, MPI_DATA_T, i, 99, MPI_COMM_WORLD);
         }
+        */
+       MPI_Scatter(A,N/numP,MPI_DATA_T,B/*+id*(N/numP) en slave*/,N/numP,MPI_DATA_T,0,MPI_COMM_WORLD);
         start=1;
-        end=N/P;
-        for(i=start;i<end;i++){
+        end=N/numP;
+        for(i=1;i<N/numP;i++){
             B[i] = (A[i-1] + A[i] + A[i+1]) * (1.0/3); 
             if (fabs(B[0]-B[i]) > precision){
-                converge[tid] = 0;
+                converge2[0] = 0;
                 i++;
                 break;
             }
@@ -96,10 +214,10 @@ int funcionDelMaster(int N, int P)
             B[i] = (A[i-1] + A[i] + A[i+1])* (1.0/3);
         }
 
-        for (i = 1; i < P; i++)
+        for (i = 1; i < numP; i++)
         {
             //Recibo parte de B calculada por slave
-            MPI_Recv(B + (N * (N / P)) * i, N * (N / P), MPI_DOUBLE, i, 97, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(B + (N * (N / numP)) * i, N * (N / numP), MPI_DOUBLE, i, 97, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             //Recibo si le dió que converge
             //*hacer*
             printf("Llego bien el resultado\n");
@@ -155,4 +273,11 @@ double dwalltime()
     gettimeofday(&tv, NULL);
     sec = tv.tv_sec + tv.tv_usec / 1000000.0;
     return sec;
+}
+void printVector(int N, DATA_T *M){
+	printf("Vector = [");
+	for(int i= 0;i<N;i++){
+		printf("%.8f, ",M[i]);
+	}
+	printf("]\n");
 }

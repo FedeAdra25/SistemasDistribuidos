13a8d9e8cc00412b2d8d7386f1bccad4e2170d15
Evaristo00 <evaristogaston@gmail.com>
2022-05-29 16:01:13 -0300

    vector se pasan bien los datos no termina

--- Paralelo/MPI/compile.sh
+++ Paralelo/MPI/compile.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 #Este script genera los archivos compilados de vector.c y matriz.c
+debug=${1:-""} # -DPRINT_VEC -DPRUEBA
+
+# Guardamos el directorio del script
+SCRIPT_DIR=$(dirname $0)
+echo "El script está en: $SCRIPT_DIR"
+echo "Todos los argumentos = $@"
+cd $SCRIPT_DIR
+
+mpicc $debug -o vector.o vecPasoEsquinas.c
+
+#mpirun -np 2 vector.o 16
